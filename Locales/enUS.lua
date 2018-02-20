21e68cd0047aacadbf4b6bf765b1ed243d9a4cd3
mizaki <jinxybob@hotmail.com>
2018-02-20 22:11:42 +0000

    Original lang try

--- Locales/enUS.lua
+++
@@ -1,6 +0,0 @@
--- Default language template
-
-local L = LibStub("AceLocale-3.0"):NewLocale("iwtb", "enUS", true)
-
-L["Request an update from a player when they join the raid"] = true
-L["Request update on player join"] = true
--- enGB.lua
+++
@@ -1,8 +0,0 @@
--- Default language template
-
-print("Loading enGB")
-
-local L = LibStub("AceLocale-3.0"):NewLocale("iWTB", "enGB", true)
-
-L["Request an update from a player when they join the raid"] = true
-L["Request update on player join"] = true
